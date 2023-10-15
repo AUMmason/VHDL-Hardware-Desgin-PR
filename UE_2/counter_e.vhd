898fdb09c7566d431e0291c8f418dfdd4dc0b9d4
Mason <underscoreaumm8@gmail.com>
2023-10-15 16:44:35 +0200

    fixed type error in counter_e.vhd

--- UE_2/counter_e.vhd
+++ UE_2/counter_e.vhd
@@ -8,6 +8,6 @@ entity counter_e is
   );
   port (
     signal clk_i, reset_i, counter_restart_strobe_i: in std_ulogic;
-    signal counter_value_o: out integer
+    signal counter_value_o: out std_ulogic_vector(BIT_WIDTH - 1 downto 0) := (others => '0')
   );
 end entity counter_e;
