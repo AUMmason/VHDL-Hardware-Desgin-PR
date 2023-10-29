f36a092647927c99d390fda5edc5d48474147061
Mason <underscoreaumm8@gmail.com>
2023-10-29 17:30:49 +0100

    Finished up work on UE 4

--- UE_4/delta_adc_a.vhd
+++ UE_4/delta_adc_a.vhd
@@ -8,7 +8,13 @@ architecture rtl of delta_adc is
   signal adc_value, next_adc_value : unsigned(BIT_WIDTH - 1 downto 0);
 begin
   
-  -- Wiring everthing up:
+  -- Explaining the use of sampling_period_i:
+
+  -- sampling_period_i defines: Period_counter_val_i since the pwm-signal 
+  -- must be created according to the time between two sample strobes
+  -- sampling_period_i is also an upper bound for the adc_value since the adc_value
+  -- must lie between 0 and Period_counter_val_i
+
   Sampling_Strobe_Module: entity work.strobe_generator(rtl) generic map(
     BIT_WIDTH => BIT_WIDTH
   ) port map (
@@ -27,8 +33,6 @@ begin
     ON_counter_val_i => adc_value,
     PWM_pin_o => PWM_o
   );
-  
-  -- adc_valid_strobe_o can only be '1' after sampling strobe has been '1'
 
   Clock: process(clk_i, reset_i)
   begin
@@ -50,11 +54,12 @@ begin
       next_adc_valid_strobe <= '1';
     
       if comparator_i = '1' and adc_value < sampling_period_i then
-        next_adc_value <= adc_value + to_unsigned(BIT_WIDTH - 1, 1);
+        next_adc_value <= adc_value + to_unsigned(1, BIT_WIDTH - 1);
       elsif comparator_i = '0' and adc_value > 0 then 
-      -- @Tutor: Why can I not use adc_value > to_unsigned(BIT_WIDTH - 1, 0); instead of adc_value > 0 ???
-        report "adc_value - 1";
-        next_adc_value <= adc_value - to_unsigned(BIT_WIDTH - 1, 1);
+     
+        -- @Tutor: Why can I not use adc_value > to_unsigned(BIT_WIDTH - 1, 0); instead of adc_value > 0 ???
+        next_adc_value <= adc_value - to_unsigned(1, BIT_WIDTH - 1);
+
       end if;
 
     else 
--- UE_4/delta_adc_tb.vhd
+++ UE_4/delta_adc_tb.vhd
@@ -9,11 +9,11 @@ end entity delta_adc_tb;
 architecture Testbench of delta_adc_tb is
   constant CLK_FREQUENCY : integer := 1000; -- Hz
   constant CLK_PERIOD : time := 1000 ms / CLK_FREQUENCY; -- T = 1/f
-  constant SAMPLING_PERIOD_SIZE : natural := 15;
+  constant SAMPLING_PERIOD_SIZE : natural := 20;
   -- Takes a natural number and specifies the desired bitlength for the bit_vectors
   constant BIT_WIDTH : natural := integer( ceil(log2(real( SAMPLING_PERIOD_SIZE ))) );
 
-  signal sampling_period : unsigned(BIT_WIDTH - 1 downto 0) := "1111";
+  signal sampling_period : unsigned(BIT_WIDTH - 1 downto 0) := to_unsigned(SAMPLING_PERIOD_SIZE, BIT_WIDTH);
   signal clk, reset : std_ulogic := '0';
   signal adc_valid_strobe, comparator, PWM_o : std_ulogic;
 begin
