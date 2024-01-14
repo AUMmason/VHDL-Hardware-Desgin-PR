a3ed233e84d68ad0d9428a13d308e33a4479ea72
Mason <underscoreaumm8@gmail.com>
2024-01-14 16:31:27 +0100

    Added increase and decrease adc_value to functionality to button_control

--- UE_8/button_control_ea.vhd
+++ UE_8/button_control_ea.vhd
@@ -8,6 +8,9 @@ use work.tilt_package.all;
 -- Central module for performing actions in debug mode and to turn on/off certain modules
 
 entity button_control is
+  generic (
+    ADC_BIT_WIDTH : natural
+  );
   port (
     -- Inputs:
     -- Switches
@@ -16,57 +19,84 @@ entity button_control is
     signal btn_increase_i, btn_decrease_i : in std_ulogic; -- Reset Button is handled in top-level-entity
     -- Board Signals
     signal clk_i, reset_i : in std_ulogic;
-
     -- Outputs:
     signal debug_led_o : out std_ulogic; -- Led to show that debug mode is enabled!
-    signal adc_value_x_o, adc_value_y_o : out std_ulogic;
-    signal adc_valid_strobe_o : out std_ulogic
+    signal adc_value_x_o, adc_value_y_o : out unsigned(ADC_BIT_WIDTH - 1 downto 0);
+    signal adc_valid_strobe_o : out std_ulogic;
+    signal enable_filter_o : out std_ulogic
   );
 end entity button_control;
 
 architecture rtl of button_control is
   constant DEBOUNCE_TIME_MS : natural := 20; -- 20 ms
-  constant ADC_BIT_WIDTH : natural := integer( ceil(log2(real( ADC_VALUE_RANGE ))) ); 
-  constant BASE_AMOUNT : natural := 1;
-  constant AMOUNT_MULTIPLYER : natural := 10;
-
+  constant ADC_INCREMENT : unsigned(ADC_BIT_WIDTH - 1 downto 0) := to_unsigned(1, ADC_BIT_WIDTH);
+  constant ADC_INCREMENT_MULTIPLIER : unsigned(ADC_BIT_WIDTH - 1 downto 0) := to_unsigned(10, ADC_BIT_WIDTH);
+  -- The 2 constants below are not in a package because similar names are used in other files
+  constant ADC_MAX_VALUE : unsigned(ADC_BIT_WIDTH - 1 downto 0) := to_unsigned(ADC_VALUE_RANGE, ADC_BIT_WIDTH);
+  constant ADC_MIN_VALUE : unsigned(ADC_BIT_WIDTH - 1 downto 0) := to_unsigned(0, ADC_BIT_WIDTH);
+  
   signal sw_enable_filter, sw_enable_debug_mode, sw_select_axis, sw_select_increase_amount : std_ulogic;
   signal btn_increase, btn_decrease : std_ulogic;
 
   signal adc_value_x, adc_value_y : unsigned(ADC_BIT_WIDTH - 1 downto 0);
   signal adc_valid_strobe : std_ulogic;
+
+  impure function set_adc_value(
+    total_value, increment_value, factor: unsigned(ADC_BIT_WIDTH - 1 downto 0);
+    enable_factor, increase, decrease: std_ulogic) 
+  return unsigned is
+    variable total : unsigned(ADC_BIT_WIDTH - 1 downto 0) := total_value;
+    variable value : unsigned(ADC_BIT_WIDTH - 1 downto 0) := increment_value;
+  begin
+    if enable_factor = '1' then 
+      value := resize(value * factor, ADC_BIT_WIDTH);
+    end if;
+    -- Assuming that only one button will be pressed at a time!
+    if increase = '1' then
+      if total + value <= ADC_MAX_VALUE then
+        total := total + value;
+      else 
+        total := ADC_MAX_VALUE;
+      end if;
+    elsif decrease = '1' then
+      if total - value >= ADC_MIN_VALUE then
+        total := total - value;
+      else 
+        total := ADC_MIN_VALUE;
+      end if;
+    end if;
+
+    return total;
+  end function;
 begin
   
+  -- TODO: ADC_VALID STROBE!
+
   debug_led_o <= sw_enable_debug_mode;
+  adc_value_x_o <= adc_value_x;
+  adc_value_y_o <= adc_value_y;
+  enable_filter_o <= sw_enable_filter;
 
   clk: process(clk_i, reset_i)
   begin
     if reset_i = '1' then
-      -- TODO: Maybe change this later to an angle where the ball can be balanced better!
-      adc_value_x <= to_unsigned(0, ADC_BIT_WIDTH);
-      adc_value_y <= to_unsigned(0, ADC_BIT_WIDTH);
+      -- TODO: Maybe change this later to an angle where the ball can be balanced better! Initialize with values corresponding to 0 degress = 150
+      adc_value_x <= to_unsigned(175, ADC_BIT_WIDTH); -- 90°
+      adc_value_y <= to_unsigned(175, ADC_BIT_WIDTH); -- 90°
       adc_valid_strobe <= '0';
     elsif rising_edge(clk_i) then
       
     end if;
   end process clk;
 
-  -- TODO implement statement for increasing / decreasing inputs / outpus
   -- Note: a Process was chosen for processing increases/decreases in adc_values since the code is more well arranged!
   btn_actions: process(sw_select_axis, sw_enable_debug_mode, sw_select_increase_amount, btn_increase, btn_decrease)
-    variable amount : unsigned(ADC_BIT_WIDTH - 1 downto 0) := to_unsigned(BASE_AMOUNT, ADC_BIT_WIDTH);
   begin
-    if sw_enable_debug_mode = '1' then
-      if sw_select_increase_amount = '1' then -- Increase ADC-Value by 10
-        amount <= resize(amount * to_unsigned(AMOUNT_MULTIPLYER, ADC_BIT_WIDTH))
-      else
-
-      end if;
-
+    if sw_enable_debug_mode = '1' then      
       if sw_select_axis = '1' then -- X-Axis is selected
-        adc_value_x <= ...  
+        adc_value_x <= set_adc_value(adc_value_x, ADC_INCREMENT, ADC_INCREMENT_MULTIPLIER, sw_select_increase_amount, btn_increase, btn_decrease);
       else -- Y-Axis is selected
-        adc_value_y <= ...
+        adc_value_y <= set_adc_value(adc_value_y, ADC_INCREMENT, ADC_INCREMENT_MULTIPLIER, sw_select_increase_amount, btn_increase, btn_decrease);
       end if;
     end if;
   end process btn_actions;
